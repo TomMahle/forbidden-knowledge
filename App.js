580adaaac121b3a2161e98ab48892cc69e8d5a69
Tom Mahle <tmahle@tempworks.com>
2020-08-09 13:52:20 -0500

    Add decrees.

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { StyleSheet, Text, View, Platform, Switch } from 'react-native';
 import { VictoryLabel, VictoryAxis } from 'victory';
 const {
   VictoryBar, VictoryChart, VictoryTheme
 } = require('victory')// Platform.OS === 'web' ? require('victory') : require('victory-native');
 
-
-const offseasonFetch = fetch('https://blaseball.com/database/offseasonSetup')
-  .then(response => response.json());
-
 export default function App() {
   const [request, setRequest] = useState({ loading: true, data: [] });
+  const blessings = 'Blessings';
+  const decrees = 'Decrees';
+  const [showDecrees, setShowDecrees] = useState(false);
   useEffect(
     () => {
-      offseasonFetch
+      fetch('https://blaseball.com/database/offseasonSetup')
+        .then(response => response.json())
         .then(jsonData => {
           setRequest({
             loading: false,
-            data: jsonData
+            data: {
+              ...jsonData,
+              bonuses: jsonData.bonuses.reverse(),
+              decrees: jsonData.decrees.reverse(),
+            }
           })
         })
     }
@@ -26,9 +30,16 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <View style={styles.switchContainer}>
+        <Text>{blessings}</Text><Switch value={showDecrees} onValueChange={setShowDecrees}/><Text>{decrees}</Text>
+      </View>
       {request.loading
         ? <Text>Loading...</Text>
-        : <VictoryChart theme={VictoryTheme.grayscale} horizontal={true} domainPadding={10}>
+        : <VictoryChart 
+          theme={VictoryTheme.grayscale} 
+          horizontal={true} 
+          domainPadding={10}
+        >
           <VictoryAxis 
             dependentAxis={true} 
             axisComponent={<View />} 
@@ -40,7 +51,7 @@ export default function App() {
           <VictoryBar
             barRatio={0.95}
             alignment="start"
-            data={request.data.bonuses.reverse()}
+            data={request.data[showDecrees ? 'decrees' : 'bonuses']}
             labels={({ datum }) => `${datum.title} (${datum.votes})`}
             y="votes"
             style={{
@@ -61,6 +72,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'lightgrey',
   },
+  switchContainer: {
+    flexDirection: 'row',
+  }
 });
